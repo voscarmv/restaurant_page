4515315e9d3e073dac576151f4a44cab60bf2ad8
Oscar Mier <voscarmv@gmail.com>
2020-07-26 14:53:19 -0500

    Added html and cleaner.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import _ from 'lodash';
 import './style.css';
 import Example from './example.jpg';
+import printMe from './print.js';
 
 function component() {
   const element = document.createElement('div');
+  const btn = document.createElement('button');
 
   // Lodash now imported from this script
   element.innerHTML = _.join(['Hello', 'webpack'], ' ');
@@ -13,6 +15,11 @@ function component() {
   myImage.src = Example;
 
   element.appendChild(myImage);
+
+  btn.innerHTML = 'Click for console message';
+  btn.onclick = printMe;
+
+  element.appendChild(btn);
 
   return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new htmlWebpackPlugin({
+      title: 'Output Management',
+    }),    
+  ],
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
