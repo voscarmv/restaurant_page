b6dc6d8d684f9681504c99a6c84235284004e3ea
Oscar Mier <voscarmv@gmail.com>
2020-07-26 15:37:56 -0500

    Added babbel

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,16 @@ module.exports = {
   module: {
     rules: [
       {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
         test: /\.css$/,
         use: [
           'style-loader',
